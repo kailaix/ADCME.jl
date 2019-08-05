2a380037b6c1f049927601d2012cbaa9ca6c5f84
Kailai Xu <kailaix@stanford.edu>
2019-08-05 11:29:59 -0700

    python 3 default

--- deps/install.sh
+++ deps/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -v  # print commands as they're executed
 
-pip3 install --user -U numpy
-pip3 install --user "tensorflow==1.14"
-pip3 install --user "tensorflow_probability==0.7"
+pip install --user -U numpy
+pip install --user "tensorflow==1.14"
+pip install --user "tensorflow_probability==0.7"
