e0e443184968524bd0aa646385bc0b700162c198
Kailai Xu <kailaix@stanford.edu>
2019-08-26 14:37:47 -0700

    remove probability dependency

--- deps/build.jl
+++ deps/build.jl
@@ -16,12 +16,10 @@ function install_tensorflow()
     end
     run(`$(PyCall.python) -m pip install --user -U numpy`)
     run(`$(PyCall.python) -m pip install --user tensorflow==1.14`)
-    run(`$(PyCall.python) -m pip install --user tensorflow_probability==0.7`)
 end
 
 try
     tf = pyimport("tensorflow")
-    tfp = pyimport("tensorflow_probability")
 catch ee
     install_tensorflow()
 end
