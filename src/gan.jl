1161b574423a94063797149ff526717efea0a008
Kailai Xu <kailaix@stanford.edu>
2019-08-30 19:19:07 -0700

    working

--- src/gan.jl
+++ src/gan.jl
@@ -56,15 +56,8 @@ function Base.:show(io::IO, gan::GAN)
     print("GAN(id=$(gan.ganid), dat(shape=$(size(gan.dat)), type=$(get_dtype(gan.dat)), latent_dim=$(gan.latent_dim), batch_size=$(gan.batch_size), $(length(gan.d_vars)) d_vars, $(length(gan.g_vars)) g_vars, $(length(gan.update)) update ops, fake_data(shape=$(size(gan.fake_data))), true_data(shape=$(size(gan.true_data))))")
 end
 ##################### GAN Library #####################
-
-"""
-D_loss, G_loss = klgan(P::PyObject, Q::PyObject, gan::GAN)
-
-return discriminator loss and generator loss for KL divergence
-`P` is the real distribution, `Q` is the generated distribution, 
-`discriminator` is a critic function that outputs values in (0,1) (e.g. the last activation function is sigmoid)
-"""
-function klgan(P::PyObject, Q::PyObject, gan::GAN)
+function klgan(gan::GAN)
+    P, Q = gan.true_data, gan.fake_data
     D_real = gan.discriminator(P, gan)
     D_fake = gan.discriminator(Q, gan)
     D_loss = -mean(log(D_real) + log(1-D_fake))
@@ -72,14 +65,8 @@ function klgan(P::PyObject, Q::PyObject, gan::GAN)
     D_loss, G_loss
 end
 
-"""
-D_loss, G_loss = gan(P::PyObject, Q::PyObject, gan::GAN)
-
-return discriminator loss and generator loss for JS divergence
-`P` is the real distribution, `Q` is the generated distribution, 
-`discriminator` is a critic function that outputs values in (0,1) (e.g. the last activation function is sigmoid)
-"""
-function jsgan(P::PyObject, Q::PyObject, gan::GAN)
+function jsgan(gan::GAN)
+    P, Q = gan.true_data, gan.fake_data
     D_real = gan.discriminator(P, gan)
     D_fake = gan.discriminator(Q, gan)
     D_loss = -mean(log(D_real) + log(1-D_fake))
@@ -87,15 +74,8 @@ function jsgan(P::PyObject, Q::PyObject, gan::GAN)
     D_loss, G_loss
 end
 
-"""
-D_loss, G_loss = wgan(P::PyObject, Q::PyObject, gan::GAN)
-
-return discriminator loss and generator loss for 1 Wasserstein
-`P` is the real distribution, `Q` is the generated distribution, 
-No constraint is imposed on discriminator
-`clamp` is required for the discriminator weights
-"""
-function wgan(P::PyObject, Q::PyObject, gan::GAN)
+function wgan(gan::GAN)
+    P, Q = gan.true_data, gan.fake_data
     D_real = gan.discriminator(P, gan)
     D_fake = gan.discriminator(Q, gan)
     D_loss = mean(D_fake)-mean(D_real)
@@ -103,14 +83,8 @@ function wgan(P::PyObject, Q::PyObject, gan::GAN)
     D_loss, G_loss
 end
 
-"""
-D_loss, G_loss = rklgan(P::PyObject, Q::PyObject, gan::GAN)
-
-return discriminator loss and generator loss for reverse KL divergence
-`P` is the real distribution, `Q` is the generated distribution, 
-`discriminator` is a critic function that outputs values in (0,1) (e.g. the last activation function is sigmoid)
-"""
-function rklgan(P::PyObject, Q::PyObject, gan::GAN)
+function rklgan(gan::GAN)
+    P, Q = gan.true_data, gan.fake_data
     D_real = gan.discriminator(P, gan)
     D_fake = gan.discriminator(Q, gan)
     G_loss = mean(log((1-D_fake)/D_fake))
@@ -118,16 +92,8 @@ function rklgan(P::PyObject, Q::PyObject, gan::GAN)
     D_loss, G_loss
 end
 
-
-"""
-D_loss, G_loss = lsgan(P::PyObject, Q::PyObject, gan::GAN)
-
-return discriminator loss and generator loss for least square
-`P` is the real distribution, `Q` is the generated distribution, 
-`discriminator` is a critic function that outputs values in (0,1) (e.g. the last activation function is sigmoid)
-1 for real, 0 for fake
-"""
-function lsgan(P::PyObject, Q::PyObject, gan::GAN)
+function lsgan(gan::GAN)
+    P, Q = gan.true_data, gan.fake_data
     D_real = gan.discriminator(P, gan)
     D_fake = gan.discriminator(Q, gan)
     D_loss = mean((D_real-1)^2+D_fake^2)
@@ -148,7 +114,7 @@ function build!(gan::GAN)
         gan.loss = eval(Meta.parse(gan.loss))
     end
     variable_scope("discriminator_$(gan.ganid)", initializer=random_uniform_initializer(0.0,1e-3)) do
-        gan.d_loss, gan.g_loss = gan.loss(gan.true_data, gan.fake_data, gan)
+        gan.d_loss, gan.g_loss = gan.loss(gan)
     end
     gan.d_vars = Array{PyObject}(get_collection("discriminator_$(gan.ganid)"))
     gan.g_vars = Array{PyObject}(get_collection("generator_$(gan.ganid)"))
