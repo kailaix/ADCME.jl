9adf259a031f4750b77cafea5cc9a8a907d7593f
Kailai Xu <kailaix@stanford.edu>
2019-09-04 23:57:18 -0700

    phase i

--- examples/JuliaOpModule.jl/DoItOp/DoItOp.cpp
+++ examples/JuliaOpModule.jl/DoItOp/DoItOp.cpp
@@ -10,6 +10,9 @@ using namespace tensorflow;
 #include "Python.h"
 // If you want to use the PyTorch feature, uncomment the following line
 // #include "la.h" 
+jl_value_t* array_type;
+uv_async_t* uv_async_cond;
+
 #include "DoItOp.h"
 
 REGISTER_OP("DoItOp")
@@ -22,6 +25,23 @@ REGISTER_OP("DoItOp")
         TF_RETURN_IF_ERROR(c->WithRank(c->input(0), 1, &x_shape));
 
         c->set_output(0, c->Vector(-1));
+
+        array_type = jl_apply_array_type((jl_value_t*)jl_float64_type, 1);
+
+
+        printf("Created array_type\n");
+        foo();
+        printf("My id = %d\n\n", jl_threadid());
+
+        PyGILState_STATE py_threadstate;
+        py_threadstate = PyGILState_Ensure();
+        jl_value_t* _cpp_async_cond_handle =
+            jl_eval_string("const _cpp_async_cond = Base.AsyncCondition(); _cpp_async_cond.handle");
+        auto s = "a = @task printjulia("+ std::to_string(reinterpret_cast<std::size_t>(&global_x))+"); schedule(a)";
+        jl_eval_string(s.c_str());
+        uv_async_cond = reinterpret_cast<uv_async_t*>(jl_unbox_voidpointer(_cpp_async_cond_handle));
+        PyGILState_Release(py_threadstate);
+
     return Status::OK();
   });
 class DoItOpOp : public OpKernel {
@@ -29,7 +49,6 @@ private:
   
 public:
   explicit DoItOpOp(OpKernelConstruction* context) : OpKernel(context) {
-
   }
 
   void Compute(OpKernelContext* context) override {    
