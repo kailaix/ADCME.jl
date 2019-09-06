2ccbb8f3befbb6d1722102e0c76bff5b582290b4
Kailai Xu <kailaix@stanford.edu>
2019-09-05 23:30:32 -0700

    revert to original version

--- examples/JuliaOpModule.jl/DoItOp/DoItOp.h
+++ examples/JuliaOpModule.jl/DoItOp/DoItOp.h
@@ -1,58 +1,20 @@
-#include <mutex>
-#include <thread>
-#include <iostream>
-
-std::mutex mu;
-void foo()
-{
-    auto id = std::this_thread::get_id();
- 
-    mu.lock();
-    std::cout << "thread " << id << " sleeping...\n";
-    mu.unlock();
- 
-}
-
-void printjulia(){
-    jl_eval_string("println(\"Evaluating Julia script\");");
-}
-
-int v;
-const double *global_x;
-double *global_y;
-
-extern "C" void Cfunction(){
-    printf("From Cfunction\n");
-    printf("%d, %f\n", v, global_x[0]);
-    // jl_eval_string("println(\"Cfunction!\")");
-}
-
 void forward(double *y, const double *x, int n){
     PyGILState_STATE py_threadstate;
     py_threadstate = PyGILState_Ensure();
-    foo();
-    // printf("My id = %d\n\n", jl_threadid());
-    v = 10;
-    printf("\n*************\n wake up julia \n*************\n");
-    global_x = x;
-    global_y = y;
-    // uv_async_send(uv_async_cond);
-
-
-    // jl_eval_string("println(1);");
-    // jl_value_t **args;
-    // JL_GC_PUSHARGS(args, 2); // args can now hold 2 `jl_value_t*` objects
-    // args[0] = (jl_value_t*)jl_ptr_to_array_1d(array_type, y, n, 0);
-    // args[1] = (jl_value_t*)jl_ptr_to_array_1d(array_type, const_cast<double*>(x), n, 0);
-    // auto fun = jl_get_function(jl_main_module, "DoIt!");
-    // if (fun==NULL){
-    //     jl_errorf("Function not found in Main module.");
-    // }
-    // else{
-    //     jl_call(fun, args, 2);
-    // }
-    // JL_GC_POP();
-    // if (jl_exception_occurred())
-    //     printf("%s \n", jl_typeof_str(jl_exception_occurred()));
+    jl_value_t* array_type = jl_apply_array_type((jl_value_t*)jl_float64_type, 1);
+    jl_value_t **args;
+    JL_GC_PUSHARGS(args, 2); // args can now hold 2 `jl_value_t*` objects
+    args[0] = (jl_value_t*)jl_ptr_to_array_1d(array_type, y, n, 0);
+    args[1] = (jl_value_t*)jl_ptr_to_array_1d(array_type, const_cast<double*>(x), n, 0);
+    auto fun = jl_get_function(jl_main_module, "DoIt!");
+    if (fun==NULL){
+        jl_errorf("Function not found in Main module.");
+    }
+    else{
+        jl_call(fun, args, 2);
+    }
+    JL_GC_POP();
+    if (jl_exception_occurred())
+        printf("%s \n", jl_typeof_str(jl_exception_occurred()));
     PyGILState_Release(py_threadstate);
 }
--- examples/JuliaOpModule.jl/DoItOp/gradtest.jl
+++ examples/JuliaOpModule.jl/DoItOp/gradtest.jl
@@ -3,50 +3,18 @@ using PyCall
 using LinearAlgebra
 using PyPlot
 using Random
-using Cxx
 Random.seed!(233)
 
 do_it_op = load_op("build/libDoItOp", "do_it_op")
 function DoIt!(x::Array{Float64}, y::Array{Float64})
-    println(y)
     x[:] = y
 end
 
-function printjulia(p::Int64)
-    wait(_cpp_async_cond)
-    # p = unsafe_load(Ptr{Float64}(Ptr{Cvoid}(p)))
-    # @show p
-    # ccall((:Cfunction, "build/libDoItOp.dylib"), Cvoid, ())
-    println("JULIA WORKS")
-end
-
-# cxx"""
-# #include <mutex>
-# #include <thread>
-# #include <iostream>
-
-# std::mutex mu;
-# void foo()
-# {
-    
-#     auto id = std::this_thread::get_id();
- 
-#     mu.lock();
-#     std::cout << "thread " << id << " sleeping...\n";
-#     mu.unlock();
- 
-# }
-# """
-# @cxx foo()
-
 # TODO: specify your input parameters
-# session_conf = tf.ConfigProto(
-#       intra_op_parallelism_threads=8,
-#       inter_op_parallelism_threads=8)
-x = Variable(ones(100))
+x = constant(rand(100))
 u = do_it_op(x)
 sess = Session()
 init(sess)
-run(sess, u);
+run(sess, u)
 
 
