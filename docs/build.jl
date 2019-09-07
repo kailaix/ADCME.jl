32f0022a52e027da6254e91f6d329f774ff1a565
Kailai Xu <kailaix@stanford.edu>
2019-09-06 22:40:08 -0700

    add docs

---
+++ docs/build.jl
@@ -0,0 +1,26 @@
+using PyCall
+function install_packages()
+    try 
+        run(`$(PyCall.python) -m pip --version`)
+    catch
+        @warn "pip is not installed, downloading and installing pip..."
+        download("https://bootstrap.pypa.io/get-pip.py", "get-pip.py")
+        run(`$(PyCall.python) get-pip.py --user`)
+        rm("get-pip.py")
+    end
+    run(`$(PyCall.python) -m pip install --user scipy`)
+    run(`$(PyCall.python) -m pip install --user matplotlib`)
+end
+
+install_packages()
+
+using Documenter
+
+makedocs(sitename="ADCME", modules=[ADCME],
+pages = Any["index.md"],
+authors = "Kailai Xu")
+
+
+deploydocs(
+    repo = "github.com/kailaix/ADCME.jl.git",
+)
