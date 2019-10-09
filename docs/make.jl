327056896c207a311af2cdf2f288a8b682fa62ba
Kailai Xu <kailaix@stanford.edu>
2019-10-08 18:44:16 -0700

    typo

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ pages = Any[
     "index.md",
     "Inverse Modeling" => [],
     "Automatic Differentiation" => ["four_types.md"],
-    "Useful Tools" => ["customop.md", "while_loop", "julia_customop.md", "extra.md"],
+    "Useful Tools" => ["customop.md", "while_loop.md", "julia_customop.md", "extra.md"],
     "Applications" => [],
     "API Reference" => ["api.md"]
 ],
