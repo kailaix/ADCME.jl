db817895223f41a5c4656882658be43cb32586bd
Kailai Xu <kailaix@stanford.edu>
2019-10-28 22:40:03 -0700

    fix test jacobian

--- src/extra.jl
+++ src/extra.jl
@@ -314,26 +314,33 @@ end
 Testing the gradients of a vector function `f`:
 `y, J = f(x)` where `y` is a vector output and `J` is the Jacobian.
 """
+"""
+    gradtest(f::Function, x0::Array{Float64}; scale::Float64 = 1.0)
+
+Testing the gradients of a vector function `f`. 
+"""
 function test_jacobian(f::Function, x0::Array{Float64}; scale::Float64 = 1.0)
     v0 = rand(Float64,size(x0))
-    γs = scale * 100 ./10 .^(1:7)
+    γs = scale ./10 .^(1:5)
     err2 = []
     err1 = []
     f0, J = f(x0)
-    for i = 1:7
+    for i = 1:5
         f1, _ = f(x0+γs[i]*v0)
-        f2, _ = f(x0-γs[i]*v0)
-        push!(err1, norm(f1-f2))
-        push!(err2, norm(f1-f2-2γs[i]*J*v0))
+        push!(err1, norm(f1-f0))
+        @show f1, f0, 2γs[i]*J*v0
+        push!(err2, norm(f1-f0-γs[i]*J*v0))
         # push!(err2, norm((f1-f2)/(2γs[i])-J*v0))
         # #@show "test ", f1, f2, f1-f2
     end
+    close("all")
     loglog(γs, err2, label="Automatic Differentiation")
     loglog(γs, err1, label="Finite Difference")
-    loglog(γs, γs.^3 * 0.5*abs(err2[1])/γs[1]^3, "--",label="\$\\mathcal{O}(\\gamma^3)\$")
+    loglog(γs, γs.^2 * 0.5*abs(err2[1])/γs[1]^2, "--",label="\$\\mathcal{O}(\\gamma^2)\$")
     loglog(γs, γs * 0.5*abs(err1[1])/γs[1], "--",label="\$\\mathcal{O}(\\gamma)\$")
     plt.gca().invert_xaxis()
     legend()
     println("Finite difference: $err1")
     println("Automatic differentiation: $err2")
+    return err1, err2
 end
