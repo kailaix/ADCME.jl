e77caa2e224a4319bf8e0e5bca8192524b60bc09
Kailai Xu <kailaix@stanford.edu>
2019-10-29 16:44:41 -0700

    fix

--- src/extra.jl
+++ src/extra.jl
@@ -300,6 +300,8 @@ function use_gpu(i::Union{Nothing,Int64}=nothing)
     if !isnothing(i) && i>=1
         i = join(collect(0:i-1),',') 
         ENV["CUDA_VISIBLE_DEVICES"] = i 
+    else
+        ENV["CUDA_VISIBLE_DEVICES"] = ""
     end
     local_device_protos = dl.list_local_devices()
     return [x.name for x in local_device_protos if x.device_type == "GPU"]
