59bc258d7556a3ad49f52655ac5cc8d4127d9d57
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-11-24 22:18:22 +0000

    build based on c83abb6

--- dev/assets/Script/SHGAN.jl
+++ dev/assets/Script/SHGAN.jl
@@ -38,33 +38,33 @@ function generator(x, isTrain=true)
     return o
 end
 
-function discriminator(x, isTrain=true)
-    local o, conv5
-    variable_scope("discriminator") do
-        # 1st hidden layer
-        conv1 = tf.layers.conv2d(x, 128, [4, 4], strides=(2, 2), padding="same")
-        lrelu1 = lrelu(conv1, 0.2)
+# function discriminator(x, isTrain=true)
+#     local o, conv5
+#     variable_scope("discriminator") do
+#         # 1st hidden layer
+#         conv1 = tf.layers.conv2d(x, 128, [4, 4], strides=(2, 2), padding="same")
+#         lrelu1 = lrelu(conv1, 0.2)
 
-        # 2nd hidden layer
-        conv2 = tf.layers.conv2d(lrelu1, 256, [4, 4], strides=(2, 2), padding="same")
-        lrelu2 = lrelu(tf.layers.batch_normalization(conv2, training=isTrain), 0.2)
+#         # 2nd hidden layer
+#         conv2 = tf.layers.conv2d(lrelu1, 256, [4, 4], strides=(2, 2), padding="same")
+#         lrelu2 = lrelu(tf.layers.batch_normalization(conv2, training=isTrain), 0.2)
 
-        # 3rd hidden layer
-        conv3 = tf.layers.conv2d(lrelu2, 512, [4, 4], strides=(2, 2), padding="same")
-        lrelu3 = lrelu(tf.layers.batch_normalization(conv3, training=isTrain), 0.2)
+#         # 3rd hidden layer
+#         conv3 = tf.layers.conv2d(lrelu2, 512, [4, 4], strides=(2, 2), padding="same")
+#         lrelu3 = lrelu(tf.layers.batch_normalization(conv3, training=isTrain), 0.2)
 
-        # 4th hidden layer
-        conv4 = tf.layers.conv2d(lrelu3, 1024, [4, 4], strides=(2, 2), padding="same")
-        lrelu4 = lrelu(tf.layers.batch_normalization(conv4, training=isTrain), 0.2)
+#         # 4th hidden layer
+#         conv4 = tf.layers.conv2d(lrelu3, 1024, [4, 4], strides=(2, 2), padding="same")
+#         lrelu4 = lrelu(tf.layers.batch_normalization(conv4, training=isTrain), 0.2)
 
-        # output layer
-        conv5 = tf.layers.conv2d(lrelu4, 1, [4, 4], strides=(1, 1), padding="valid")
-        o = tf.nn.sigmoid(conv5)
+#         # output layer
+#         conv5 = tf.layers.conv2d(lrelu4, 1, [4, 4], strides=(1, 1), padding="valid")
+#         o = tf.nn.sigmoid(conv5)
 
         
-    end
-    return o, conv5
-end
+#     end
+#     return o, conv5
+# end
 
 let fixed_z_ = randn(25, 1, 1, 100)
     global show_result
@@ -97,7 +97,8 @@ end
 
 # training parameters
 batch_size = 100
-lr = 0.0002
+# lr = 0.0002
+lr = 0.001
 train_epoch = 20
 
 if !isdir("MNIST_data")
@@ -119,54 +120,54 @@ z = placeholder(Float32, shape=(nothing, 1, 1, 100))
 isTrain = placeholder(Bool)
 
 G_z = generator(z, isTrain)
-# networks : discriminator
-D_real, D_real_logits = discriminator(x, isTrain)
-D_fake, D_fake_logits = discriminator(G_z, isTrain)
 
-# loss for each network
-D_loss_real = mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_real_logits, labels=tf.ones([batch_size, 1, 1, 1])))
-D_loss_fake = mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.zeros([batch_size, 1, 1, 1])))
-D_loss = D_loss_real + D_loss_fake
-G_loss = mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_fake_logits, labels=tf.ones([batch_size, 1, 1, 1])))
+vx = cast(Float64, reshape(x, -1, 64*64))
+vz = cast(Float64, reshape(G_z, -1, 64*64))
+reg = 100.0
+G_loss = empirical_sinkhorn(vx, vz, dist=(x,y)->dist(x,y,1), reg=reg)
+# error()
 
 
-D_vars = [x for x in get_collection() if startswith(x.name, "discriminator")]
 G_vars = [x for x in get_collection() if startswith(x.name, "generator")]
-
 control_dependencies(get_collection(tf.GraphKeys.UPDATE_OPS)) do 
-    global D_optim = AdamOptimizer(lr, beta1=0.5).minimize(D_loss, var_list=D_vars)
     global G_optim = AdamOptimizer(lr, beta1=0.5).minimize(G_loss, var_list=G_vars)
 end
 
 sess = Session(); init(sess)
 train_set = run(sess, tf.image.resize_images(mnist[1].images, [64, 64]))
+train_set = (train_set .- 0.5) / 0.5
+# # error()
+# x_ = train_set[1:batch_size,:,:,:]
+# z_ = randn(batch_size, 1, 1, 100)
+# run(sess, G_loss, feed_dict=Dict(x=>x_, z=>z_, isTrain=>true))
+# run(sess, dist(vx, vz, 1), feed_dict=Dict(x=>x_, z=>z_, isTrain=>true))
+
 num_examples = size(train_set, 1)
 
-loss_d = []
 loss_g = []
 for epoch = 1:train_epoch
     for iter = 1:div(num_examples, batch_size)
-        x_ = train_set[(iter-1)*batch_size+1:iter*batch_size,:,:,:]
-        z_ = randn(batch_size, 1, 1, 100)
-        loss_d_, _ = run(sess, [D_loss, D_optim], feed_dict=Dict(x=>x_, z=>z_, isTrain=>true))
+        if iter%50==1
+            show_result("figures2/$(epoch)_$(iter)")
+        end
 
+        x_ = train_set[(iter-1)*batch_size+1:iter*batch_size,:,:,:]
         z_ = randn(batch_size, 1, 1, 100)
         loss_g_, _ = run(sess, [G_loss, G_optim], feed_dict=Dict(x=>x_, z=>z_, isTrain=>true))
         
-        println("[$epoch, #$iter] $loss_d_, $loss_g_")
-        push!(loss_d, loss_d_)
+        println("[$epoch, #$iter] $loss_g_")
         push!(loss_g, loss_g_)
-        show_result("figures/$(epoch)_$(iter)")
+        
     end
-
 end
 
-close("all")
-plot(loss_d, label="Discriminator")
-plot(loss_g, label="Generator")
-xlabel("Iterations")
-ylabel("Loss")
-savefig("figures/loss.png")
 
-writedlm("figures/lossd.txt", loss_d)
-writedlm("figures/lossg.txt", loss_g)
+# close("all")
+# plot(loss_d, label="Discriminator")
+# plot(loss_g, label="Generator")
+# xlabel("Iterations")
+# ylabel("Loss")
+# savefig("figures/loss.png")
+
+# writedlm("figures/lossd.txt", loss_d)
+# writedlm("figures/lossg.txt", loss_g)
