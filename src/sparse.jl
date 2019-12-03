5d77bc2044aa0dd5022d6a5e173df35a93e220bd
kailaix <kailaix@hotmail.com>
2019-12-03 00:07:36 -0800

    Update sparse.jl

--- src/sparse.jl
+++ src/sparse.jl
@@ -304,8 +304,8 @@ end
 
 See [`SparseAssembler`](@ref) for an example.
 
-!!! Note
-    `accumulate` returns a `op::PyObject`. Only when `op` is executed, the nonzero values are populated into the sparse matrix. 
+!!! note
+    The function `accumulate` returns a `op::PyObject`. Only when `op` is executed, the nonzero values are populated into the sparse matrix. 
 """
 function accumulate(handle::PyObject, row::Union{PyObject, <:Integer}, cols::Union{PyObject, Array{<:Integer}}, 
     vals::Union{PyObject, Array{<:Real}})
