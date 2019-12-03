8fd6e9b658ed7462a9a9bf8d8b50556c8fb9933e
Kailai Xu <kailaix@stanford.edu>
2019-12-02 22:36:22 -0800

    bug in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ pages = Any[
     "inverse_modeling.md",
     "Manual" => ["array.md", "sparse.md", "newton_raphson.md", "parallel.md", "ode.md"],
     "Resources" => ["customop.md", "while_loop.md",
-            "julia_customop.md", "pytorchnn.md", "extra.md", "ot.md". "resource_manager.md"],
+            "julia_customop.md", "pytorchnn.md", "extra.md", "ot.md", "resource_manager.md"],
     "Applications" => ["apps_ana.md", "apps_levy.md", "apps_constitutive_law.md"],
     "api.md"
 ],
