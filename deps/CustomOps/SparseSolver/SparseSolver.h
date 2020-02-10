27ace187571f0e4dbd93fa9d91bb6d4aa74f5563
Kailai Xu <kailaix@stanford.edu>
2020-02-09 23:24:24 -0800

    improve sparse solver speed

--- deps/CustomOps/SparseSolver/SparseSolver.h
+++ deps/CustomOps/SparseSolver/SparseSolver.h
@@ -2,6 +2,9 @@
 #include <eigen3/Eigen/SparseLU>
 #include <vector>
 #include <iostream>
+// #include <chrono> s
+// using namespace std::chrono; 
+
 using namespace std;
 typedef Eigen::SparseMatrix<double> SpMat;
 typedef Eigen::Triplet<double> T;
@@ -10,6 +13,8 @@ typedef Eigen::Triplet<double> T;
 template<typename SOLVER>
 void forward(double *u, const int64 *ii, const int64 *jj, const double *vv, int64 nv, 
                    const double *f,  int64 d){
+    // std::cout << "*************" << std::endl;
+    // auto start0 = high_resolution_clock::now();
     vector<T> triplets;
     Eigen::Map<const Eigen::VectorXd> rhs(f, d); 
     for(int64 i=0;i<nv;i++){
@@ -18,11 +23,31 @@ void forward(double *u, const int64 *ii, const int64 *jj, const double *vv, int6
     }
     SpMat A(d, d);
     A.setFromTriplets(triplets.begin(), triplets.end());
+    // auto start1 = high_resolution_clock::now();
+    // auto duration = duration_cast<microseconds>(start1 - start0); 
+    // std::cout << duration.count() << std::endl;
+    
     SOLVER solver;
+
+    // auto start2 = high_resolution_clock::now();
+    // auto duration1 = duration_cast<microseconds>(start2 - start1); 
+    // std::cout << duration1.count() << std::endl;
+
+
     solver.analyzePattern(A);
     solver.factorize(A);
-    auto x = solver.solve(rhs);
-    for(int64 i=0;i<d;i++) u[i] = x[i];
+    Eigen::VectorXd x = solver.solve(rhs);
+
+    // auto start3 = high_resolution_clock::now();
+    // auto duration2 = duration_cast<microseconds>(start3 - start2); 
+    // std::cout << duration2.count() << std::endl;
+
+
+    for(int i=0;i<(int)d;i++) u[i] = x[i]; // very slow
+
+    // auto start4 = high_resolution_clock::now();
+    // auto duration3 = duration_cast<microseconds>(start4 - start3); 
+    // std::cout << duration3.count() << std::endl;
 }
 
 template<typename SOLVER>
@@ -42,7 +67,7 @@ void backward(double *grad_f, double *grad_vv, const double *grad_u,
     SOLVER solver;
     solver.analyzePattern(B);
     solver.factorize(B);
-    auto x = solver.solve(g);
+    Eigen::VectorXd x = solver.solve(g);
     // cout << x << endl;
     for(int64 i=0;i<nv;i++) grad_vv[i] = 0.0;
     for(int64 i=0;i<nv;i++){
