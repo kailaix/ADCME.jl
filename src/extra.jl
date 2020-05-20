4b2c557aac6527e4ea4e9002d9deddda66740126
Kailai Xu <kailaix@stanford.edu>
2020-05-20 04:48:38 -0700

    fix make blas

--- src/extra.jl
+++ src/extra.jl
@@ -473,17 +473,22 @@ end
 
 
 
-function _make_blas()
+function _make_blas(blas_binary)
     if Sys.iswindows()
         if isfile(joinpath(ADCME.LIBDIR, "openblas.lib"))
             return 
         end 
+        if blas_binary
+            @info "Downloading prebuilt blas from Github. If you encounter any problem with openblas when using adept, run `install_adept(blas_binary=false)` to compile from source"
+            download("https://github.com/kailaix/tensorflow-1.15-include/releases/download/v0.1.0/openblas.lib", joinpath(ADCME.LIBDIR, "openblas.lib"))
+        end
         @info "You are building openblas from source on Windows, and this process may take a long time.
 Alternatively, you can place your precompiled binary to $(joinpath(ADCME.LIBDIR, "openblas.lib"))"
         PWD = pwd()
         download("https://github.com/xianyi/OpenBLAS/archive/v0.3.9.zip", joinpath(ADCME.LIBDIR, "OpenBlas.zip"))
         cd(ADCME.LIBDIR)
         run(`cmd /c unzip OpenBLAS.zip`)
+        rm("OpenBlas", force=true, recursive=true)
         run(`cmd /c ren OpenBlas-0.3.9 OpenBlas`)
         rm("OpenBlas.zip")
         cd("OpenBlas")
@@ -491,7 +496,7 @@ Alternatively, you can place your precompiled binary to $(joinpath(ADCME.LIBDIR,
         cd("build")
         ADCME.cmake(CMAKE_ARGS="-DCMAKE_Fortran_COMPILER=flang -DBUILD_WITHOUT_LAPACK=no -DNOFORTRAN=0 -DDYNAMIC_ARCH=ON")
         ADCME.make()
-        cd("../lib/Release")
+        cd("../build/lib/Release")
         mv("openblas.lib", joinpath(ADCME.LIBDIR, "openblas.lib"))
         cd(PWD)
     else 
@@ -512,7 +517,7 @@ end
 
 Install adept-2 library: https://github.com/rjhogan/Adept-2
 """
-function install_adept(force::Bool=false)
+function install_adept(force::Bool=false; blas_binary::Bool = true)
     PWD = pwd()
     cd(ADCME.LIBDIR)
     if force 
@@ -523,9 +528,9 @@ function install_adept(force::Bool=false)
         LibGit2.clone("https://github.com/ADCMEMarket/Adept-2", "Adept-2")
     end
     cd("Adept-2/adept")
-    _make_blas()
+    _make_blas(blas_binary)
     try
-        if (!isfile("$(LIBDIR)/libadept.so") && !isfile("$(LIBDIR)/libadept.dylib")) || force
+        if (!isfile("$(LIBDIR)/libadept.so") && !isfile("$(LIBDIR)/libadept.dylib")) && !isfile("$(LIBDIR)/libadept.lib") || force
             @info """Copy "$(@__DIR__)/../deps/AdeptCMakeLists.txt" to "$(joinpath(pwd(), "CMakeLists.txt"))" ... """
             cp("$(@__DIR__)/../deps/AdeptCMakeLists.txt", "./CMakeLists.txt", force=true)
             @info """Remove $(joinpath(pwd(), "build")) ... """
