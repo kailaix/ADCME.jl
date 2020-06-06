d9802d5c5849c05d470647a50ea5c6c1f1b8b505
kailaix <kailaix@hotmail.com>
2020-06-06 01:19:40 -0700

    update flux

--- src/optim_flux.jl
+++ src/optim_flux.jl
@@ -414,28 +414,5 @@ function apply!(o::NADAM, x, Δ)
   return Δ
 end
 
-"""
-    ADAMW(η = 0.001, β::Tuple = (0.9, 0.999), decay = 0)
-
-[ADAMW](https://arxiv.org/abs/1711.05101) is a variant of ADAM fixing (as in repairing) its
-weight decay regularization.
-
-# Parameters
-- Learning rate (`η`): Amount by which gradients are discounted before updating
-                       the weights.
-- Decay of momentums (`β::Tuple`): Exponential decay for the first (β1) and the
-                                   second (β2) momentum estimate.
-- `decay`: Decay applied to weights during optimisation.
-
-# Examples
-```julia
-opt = ADAMW()
-
-opt = ADAMW(0.001, (0.89, 0.995), 0.1)
-```
-"""
-ADAMW(η = 0.001, β = (0.9, 0.999), decay = 0) =
-  Optimiser(ADAM(η, β), WeightDecay(decay))
-
 
 end 
