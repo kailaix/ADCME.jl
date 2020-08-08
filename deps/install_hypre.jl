5d48b336eeb104c50b1c46c3e199621f8694dde1
Kailai Xu <kailaix@hotmail.com>
2020-08-08 15:10:19 -0700

    update install script for hypre and openmpi

---
+++ deps/install_hypre.jl
@@ -0,0 +1,41 @@
+using BinDeps
+using ADCME
+using CMake
+
+@BinDeps.setup 
+
+const libhypre = library_dependency("libhypre", aliases=["libhypre", "libHYPRE"])
+const hyprever = "2.19.0"
+provides(Sources, 
+    URI("https://github.com/kailaix/hypre/archive/v$hyprever.tar.gz"),
+    libhypre, unpacked_dir = "hypre-$hyprever")
+
+libhypre.context.dir = ADCME.PREFIXDIR
+cmakedir = joinpath(srcdir(libhypre), "hypre-$hyprever", "src", "build")
+cc = ADCME.CC
+cxx = ADCME.CXX
+CONDA_ROOT = joinpath(ADCME.BINDIR, "..")
+
+provides(SimpleBuild,
+(@build_steps begin
+    GetSources(libhypre)
+    CreateDirectory(cmakedir)
+    @build_steps begin
+        ChangeDirectory(cmakedir)
+        `$cmake -DHYPRE_SHARED:BOOL=ON -DHYPRE_INSTALL_PREFIX:PATH=$CONDA_ROOT -DCMAKE_C_COMPILER:FILEPATH=$(cc) -DCMAKE_CXX_COMPILER:FILEPATH=$(cxx) ..`
+        `$cmake -L ..`
+        MakeTargets(".", ["all"])
+        MakeTargets(".", ["install"])
+        FileRule(joinpath(ADCME.LIBDIR, "libhypre.so"),
+            @build_steps begin
+                `cp $(CONDA_ROOT)/lib64/libHYPRE.so $(ADCME.LIBDIR)/libhypre.so`
+            end
+        )
+    end
+end),
+libhypre,
+os = :Linux)
+
+BinDeps.execute(libhypre, BinDeps.SimpleBuild)
+
+
---
+++ deps/install_openmpi.jl
@@ -0,0 +1,27 @@
+using BinDeps
+using ADCME
+
+@BinDeps.setup 
+
+libmpi = library_dependency("libmpi")
+provides(Sources, 
+    URI("https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.4.tar.gz"),
+    libmpi)
+libmpi.context.dir = ADCME.PREFIXDIR
+CONDA_ROOT = joinpath(ADCME.BINDIR, "..")
+cmakebuilddir = joinpath(srcdir(libmpi), "openmpi-4.0.4", "build")
+cc = ADCME.CC
+cxx = ADCME.CXX
+CONDA_ROOT = joinpath(ADCME.BINDIR, "..")
+provides(SimpleBuild, (@build_steps begin
+    GetSources(libmpi)
+    CreateDirectory(cmakebuilddir)
+    @build_steps begin
+        ChangeDirectory(cmakebuilddir)
+        # `../configure CC=$cc CXX=$cxx --enable-mpi-thread-multiple --prefix=$CONDA_ROOT --enable-mpirun-prefix-by-default --with-mpi-param-check=always`
+        `make -j all`
+        `make install`
+    end
+end),
+libmpi, os=:Unix)
+BinDeps.execute(libmpi, BinDeps.SimpleBuild)
