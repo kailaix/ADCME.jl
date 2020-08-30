3985789f2b4c6c831a1b4a28995b9ef51e73ee25
Kailai Xu <kailaix@hotmail.com>
2020-08-29 20:58:56 -0700

    add doc

--- src/mpi.jl
+++ src/mpi.jl
@@ -418,6 +418,20 @@ function mpi_SparseTensor(indices::PyObject, values::PyObject, ilower::Int64, iu
     mpi_SparseTensor(rows, ncols, cols, out, ilower, iupper, N, oplibpath)
 end 
 
+@doc raw"""
+    mpi_SparseTensor(rows::Union{Array{Int32,1}, PyObject}, ncols::Union{Array{Int32,1}, PyObject}, cols::Union{Array{Int32,1}, PyObject},
+        vals::Union{Array{Float64,1}, PyObject}, ilower::Int64, iupper::Int64, N::Int64)
+
+Create a $N\times N$ distributed sparse tensor `A` for the current MPI processor. The current MPI processor owns rows with indices `[ilower, iupper]`.
+The submatrix is specified using the CSR format. 
+
+- `rows`: an array indicating the rows that contain nonzero values. Note `rows ≥ ilower`. 
+- `ncols`: an array indicating the number of nonzero values for each row in `rows`. 
+- `cols`: the column indices for nonzero values. Its length is $\sum_{i=1}^{\mathrm{ncols}} \mathrm{ncols}_i$
+- `vals`: the nonzero values corresponding to each column index in `cols`
+
+Note that by default the indices are zero-based. 
+"""
 function mpi_SparseTensor(rows::Union{Array{Int32,1}, PyObject}, ncols::Union{Array{Int32,1}, PyObject}, cols::Union{Array{Int32,1}, PyObject},
      vals::Union{Array{Float64,1}, PyObject}, ilower::Int64, iupper::Int64, N::Int64)
     @assert ilower >=0
