3d0c6092ab617e2579ebe2ab8eb697eab11d5d1c
Kailai Xu <kailaix@hotmail.com>
2020-10-06 14:27:10 -0700

    add load_library

--- src/toolchain.jl
+++ src/toolchain.jl
@@ -165,7 +165,7 @@ end
 """
     get_library(filename::AbstractString)
 
-Returns a valid library file. For example, for `filename = adcme`, we have 
+Returns a valid library file. For example, for `filename = "adcme"`, we have 
 
 - On MacOS, the function returns `libadcme.dylib`
 - On Linux, the function returns `libadcme.so`
