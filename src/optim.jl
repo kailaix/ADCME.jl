eb3c2dc3b2eb9fb9c2b6cebb5f72fe6fbe93ad3c
Kailai Xu <kailaix@hotmail.com>
2020-11-10 14:14:40 -0800

    add topo example

--- src/optim.jl
+++ src/optim.jl
@@ -700,7 +700,7 @@ import Ipopt
 x = Variable(rand(2))
 loss = (1-x[1])^2 + 100(x[2]-x[1]^2)^2
 
-function opt(f, g, fg, x0)
+function opt(f, g, fg, x0, kwargs...)
     prob = createProblem(2, -100ones(2), 100ones(2), 0, Float64[], Float64[], 0, 0,
                      f, (x,g)->nothing, (x,G)->g(G, x), (x, mode, rows, cols, values)->nothing, nothing)
     prob.x = x0 
