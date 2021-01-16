1a9493a2b1c6eafc67b5c9a5148c0b67fb192de3
Kailai Xu <kailaix@hotmail.com>
2021-01-16 02:05:05 -0800

    add weak scaling

---
+++ docs/src/assets/Codes/MPI/apps/weak_scaling_benchmark/benchmark.sh
@@ -0,0 +1,12 @@
+salloc --nodes=20
+
+for n in 1 4 9 16 25 36 49 64
+do 
+    mpirun -n $n --map-by node:pe=1 --report-bindings julia benchmark.jl 1
+done 
+
+for n in 1 4 9 16 
+do 
+    mpirun -n $n --map-by node:pe=4 --report-bindings julia benchmark.jl 4
+done 
+
