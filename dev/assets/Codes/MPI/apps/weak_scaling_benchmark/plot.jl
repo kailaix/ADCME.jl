ed311286285f4c5f17b0a0dda069c1d7142fb463
Documenter.jl <documenter@juliadocs.github.io>
2021-01-16 23:54:21 +0000

    build based on af5dbc3

--- dev/assets/Codes/MPI/apps/weak_scaling_benchmark/plot.jl
+++ dev/assets/Codes/MPI/apps/weak_scaling_benchmark/plot.jl
@@ -1,26 +1,32 @@
-using DelimitedFiles
 using PyPlot
-using PyCall
-mpl = pyimport("tikzplotlib")
+using DelimitedFiles
 
 R = readdlm("result.txt")
+n = sqrt.([1, 4, 9, 16, 25])
 
 close("all")
-plot(R[1:8,1], R[1:8,3], "r-", label = "1 Core")
-plot(R[1:8,1], R[1:8,5], "r--", label = "Hypre Linear Solver (1 Core)")
-plot(R[9:12,1], R[9:12,3], "g-", label = "4 Cores")
-plot(R[9:12, 1], R[9:12,5], "g--", label = "Hypre Linear Solver (4 Cores)")
-xlabel("MPI Processors")
+plot(n, R[1:5, 3], "or-", label = "1 Core")
+plot(n[1:4], R[6:9, 3], "xg-", label = "2 Cores")
+plot(n[1:3], R[10:12, 3], "^b-", label = "3 Cores")
+plot(n, R[1:5, 5], "or--")
+plot(n[1:4], R[6:9, 5], "xg--")
+plot(n[1:3], R[10:12, 5], "^b--")
+legend()
+title("Weak Scaling for Forward Computation")
+xlabel("\$N\$")
 ylabel("Time (sec)")
-title("Forward Computation")
 savefig("forward_weak.png")
 
+
 close("all")
-plot(R[1:8,1], R[1:8,4], "r-", label = "1 Core")
-plot(R[1:8,1], R[1:8,6], "r--", label = "Hypre Linear Solver (1 Core)")
-plot(R[9:12,1], R[9:12,4], "g-", label = "4 Cores")
-plot(R[9:12, 1], R[9:12,6], "g--", label = "Hypre Linear Solver (4 Cores)")
-xlabel("MPI Processors")
+plot(n, R[1:5, 4], "or-", label = "1 Core")
+plot(n[1:4], R[6:9, 4], "xg-", label = "2 Cores")
+plot(n[1:3], R[10:12, 4], "^b-", label = "3 Cores")
+plot(n, R[1:5, 6], "or--")
+plot(n[1:4], R[6:9, 6], "xg--")
+plot(n[1:3], R[10:12, 6], "^b--")
+legend()
+title("Strong Scaling for Gradient Backpropagation")
+xlabel("\$N\$")
 ylabel("Time (sec)")
-title("Gradient Backpropagation")
 savefig("backward_weak.png")
