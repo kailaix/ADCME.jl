a2e37edc985b0f8c314b39bebf9f8acf6292eee3
Documenter.jl <documenter@juliadocs.github.io>
2021-01-16 13:00:03 +0000

    build based on 7eec960

--- dev/assets/Codes/MPI/apps/weak_scaling_benchmark/benchmark.sh
+++ dev/assets/Codes/MPI/apps/weak_scaling_benchmark/benchmark.sh
@@ -2,11 +2,11 @@
 
 for n in 1 4 9 16 25 36 49 64
 do 
-    mpirun -n $n --map-by node:pe=1 --report-bindings julia benchmark.jl 1
+    /home/kailaix/.julia/adcme/bin/mpirun -n $n --map-by node:pe=1 --report-bindings julia benchmark.jl 1
 done 
 
 for n in 1 4 9 16 
 do 
-    mpirun -n $n --map-by node:pe=4 --report-bindings julia benchmark.jl 4
+    /home/kailaix/.julia/adcme/bin/mpirun -n $n --map-by node:pe=4 --report-bindings julia benchmark.jl 4
 done 
 
