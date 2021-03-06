7fcb673b1ec95c152df9906d979e264b7ee0d42a
Kailai Xu <kailaix@hotmail.com>
2021-03-06 11:03:06 -0800

    fix typo

--- src/layers.jl
+++ src/layers.jl
@@ -238,26 +238,26 @@ random initializers are provided
 - `xavier` (default). It is useful for `tanh` fully connected neural network. 
 
 ```
-W^l_i \sim \sqrt{\frac{1}{n_{l-1}}}
+W^l_i \sim \mathcal{N}\left(0, \sqrt{\frac{1}{n_{l-1}}\right)
 ```
 
 
 - `xavier_avg`. A variant of `xavier`
 
 ```math
-W^l_i \sim \sqrt{\frac{2}{n_l + n_{l-1}}}
+W^l_i \sim \mathcal{N}\left(0, \sqrt{\frac{2}{n_l + n_{l-1}}}\right)
 ```
 
 - `he`. This is the activation aware initialization of weights and helps mitigate the problem
 of vanishing/exploding gradients. 
 
-$$W^l_i \sim \sqrt{\frac{2}{n_{l-1}}}$$
+$$W^l_i \sim \mathcal{N}\left(0, \sqrt{\frac{2}{n_{l-1}}}\right)$$
 
 # Example
 ```julia
 x = constant(rand(10,30))
-θ = ae_init([30, 20, 20, 5])
-y = ae(x, [20, 20, 5], θ) # 10×5
+θ = fc_init([30, 20, 20, 5])
+y = fc(x, [20, 20, 5], θ) # 10×5
 ```
 """
 function ae_init(output_dims::Array{Int64}; T::Type=Float64, method::String="xavier")
