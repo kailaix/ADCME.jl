020f9dc0c45889fa51078d561d5d51b6ad1718d7
Kailai Xu <kailaix@hotmail.com>
2021-03-16 17:33:13 -0700

    add examples

--- src/gpu.jl
+++ src/gpu.jl
@@ -3,7 +3,16 @@ export gpu_info, get_gpu, has_gpu
 """
     gpu_info()
 
-Returns the CUDA and GPU information. 
+Returns the CUDA and GPU information. An examplary output is 
+```
+- NVCC: /usr/local/cuda/bin/nvcc
+- CUDA library directories: /usr/local/cuda/lib64
+- Latest supported version of CUDA: 11000
+- CUDA runtime version: 10010
+- CUDA include_directories: /usr/local/cuda/include
+- CUDA toolkit directories: /home/kailaix/.julia/adcme/pkgs/cudatoolkit-10.0.130-0/lib:/home/kailaix/.julia/adcme/pkgs/cudnn-7.6.5-cuda10.0_0/lib
+- Number of GPUs: 7
+```
 """
 function gpu_info()
     NVCC = "missing"
@@ -54,7 +63,9 @@ end
 """
     get_gpu()   
 
-Returns the compiler information for GPUs. 
+Returns the compiler information for GPUs. An examplary output is 
+> (NVCC = "/usr/local/cuda/bin/nvcc", LIB = "/usr/local/cuda/lib64", INC = "/usr/local/cuda/include", TOOLKIT = "/home/kailaix/.julia/adcme/pkgs/cudatoolkit-10.0.130-0/lib", CUDNN = "/home/kailaix/.julia/adcme/pkgs/cudnn-7.6.5-cuda10.0_0/lib")
+
 """
 function get_gpu()
     NVCC = missing 
